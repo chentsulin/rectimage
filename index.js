1b0396231db85641e3bab7c903859bdc7618c637
C.T. Lin <chentsulin@gmail.com>
2015-04-15 00:27:03 +0800

    init index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = function rectimage(image, opts) {
+  opts = opts || {};
+
+  if (!image.complete) {
+    return image.onload = rectimage.bind(null, image, opts);
+  }
+
+  var canvas = document.createElement('canvas');
+  canvas.id = 'crop-canvas';
+  var context = canvas.getContext('2d');
+
+  var width = image.width;
+  var height = image.height;
+
+  var minLength;
+  var minSide;
+
+  var diff;
+  var xOffset, yOffset;
+
+  if (width < height) {
+    minSide = 'width';
+    minLength = width;
+  } else {
+    minSide = 'height';
+    minLength = height;
+  }
+
+  if (minSide === 'width') {
+    diff = height - width;
+    xOffset = 0;
+    yOffset = diff / 2;
+  } else if (minSide === 'height') {
+    diff = width - height;
+    xOffset = diff / 2;
+    yOffset = 0;
+  }
+
+  canvas.width = minLength;
+  canvas.height = minLength;
+
+  context.drawImage(image,
+    xOffset, yOffset, minLength, minLength,
+    0, 0, minLength, minLength
+  );
+
+  if (opts.canvas) {
+    return canvas;
+  } else {
+    return (new Image).src = canvas.toDataURL();
+  }
+};
+
+
